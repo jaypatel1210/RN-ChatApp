235feec6f2831fdc62454091c49a10ae1b941af9
Jay Patel <jd4215124@gmail.com>
2021-09-27 22:43:37 +0530

    Readme File added

---
+++ README.md
@@ -0,0 +1,7 @@
+## Demo
+
+- Screen shots of different Screens
+  </br>
+  <img src="images/demo/Screen1.jpg" alt="Screen 1" width="250">
+  <img src="images/demo/Screen2.jpg" alt="Screen 2" width="250">
+  <img src="images/demo/Screen3.jpg" alt="Screen 3" width="250">
--- src/App.js
+++ src/App.js
@@ -1,10 +1,45 @@
-import React from 'react';
+import React, {useEffect} from 'react';
 import Routes from './Routes';
+import {Alert} from 'react-native';
 
 import {Provider} from 'react-redux';
 import store from './store';
 
+// ONE SIGNAL FOR NOTIFICATION
+
+import OneSignal from 'react-native-onesignal';
+
 const App = () => {
+  const os = async => {
+    OneSignal.setAppId('');
+    OneSignal.setLogLevel(6, 0);
+
+    //Method for handling notifications received while app in foreground
+    OneSignal.setNotificationWillShowInForegroundHandler(
+      notificationReceivedEvent => {
+        console.log(
+          'OneSignal: notification will show in foreground:',
+          notificationReceivedEvent,
+        );
+        let notification = notificationReceivedEvent.getNotification();
+        console.log('notification: ', notification);
+        const data = notification.additionalData;
+        console.log('additionalData: ', data);
+        // Complete with null means don't show a notification.
+        notificationReceivedEvent.complete(notification);
+      },
+    );
+
+    //Method for handling notifications opened
+    OneSignal.setNotificationOpenedHandler(notification => {
+      console.log('OneSignal: notification opened:', notification);
+    });
+  };
+
+  useEffect(() => {
+    os();
+  }, []);
+
   return (
     <Provider store={store}>
       <Routes />
